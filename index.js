dfc4f4cef44c5a3f9af82db6f0a61eacff84ee84
Victoria Mialik <victoriya.golub1@gmail.com>
2022-08-18 13:45:51 +0300

    completed tasks with cycles

--- index.js
+++ index.js
@@ -235,3 +235,65 @@ for (let n = 1000; n >= 50 ;num++) {
   alert (`Количество итераций: ${num}`);
   alert (`Число: ${n}`);
 } 
+
+// SWITCH
+
+// Task1
+let browser = 'Chrome';
+  if (browser == 'Edge') {
+    alert( "You've got the Edge!" );
+  } else if (browser == 'Chrome') {
+    alert( 'Okay we support these browsers too' );
+  } else if (browser == 'Firefox') {
+    alert( 'Okay we support these browsers too' );
+  }else if (browser == 'Safari') {
+    alert( 'Okay we support these browsers too' );
+  }else if (browser == 'Opera') {
+    alert( 'Okay we support these browsers too' );
+  } else {
+    alert( 'We hope that this page looks ok!' );
+  }
+
+// Task2
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch (number) {
+  case 0:
+    alert('Вы ввели число 0');
+  break;
+
+  case 1:
+    alert('Вы ввели число 1');
+  break;
+
+  case 2:
+  case 3:
+    alert('Вы ввели число 2, а может и 3');
+  break;
+}
+
+// Task3
+let num = +prompt ("Введите значение:");
+let result;
+
+switch (num) {
+  case 1:
+  result = 'зима';
+  alert(result);
+  break;
+
+  case 2:
+  result = 'весна';
+  alert(result);
+  break;
+
+  case 3:
+  result = 'лето';
+  alert(result);
+  break;
+
+  case 4:
+  result = 'осень';
+  alert(result);
+  break;
+} 
